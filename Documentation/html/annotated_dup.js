15a995f56d51a92ecf2e001f5e2f11df7d9cffd8
MuhammedEl-Sayed <g4mereaper@gmail.com>
2021-08-03 19:31:50 +0200

    Editing comments

--- Documentation/html/annotated_dup.js
+++
@@ -1,8 +0,0 @@
-var annotated_dup =
-[
-    [ "ContentSizeCalc", "class_content_size_calc.html", null ],
-    [ "FlexboxEditor", "class_flexbox_editor.html", "class_flexbox_editor" ],
-    [ "FlexChildren", "class_flex_children.html", "class_flex_children" ],
-    [ "FlexChildrenEditor", "class_flex_children_editor.html", "class_flex_children_editor" ],
-    [ "FlexContainer", "class_flex_container.html", "class_flex_container" ]
-];
--- Documentation/html/search/all_7.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['mainaxisalignment_44',['MainAxisAlignment',['../class_flex_container.html#a68ade8f33e523380119ce2f6bc3e1ff9',1,'FlexContainer']]],
-  ['mainsizedetermination_45',['MainSizeDetermination',['../class_flex_container.html#a97217b90865d1c8ed101d1ef19c2b6cb',1,'FlexContainer']]]
-];
